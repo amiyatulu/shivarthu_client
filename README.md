7ae147e9443de69f05a8debc5741e3866e4491d5
Amiya Behera <amiyatulu@gmail.com>
2022-12-07 19:33:40 +0530

    fileupload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Shivarthu client based on yew
+
+## rpc call to substrate
+## transaction
+## strorage call
+## file upload
--- src/components/pages/fileupload.rs
+++ src/components/pages/fileupload.rs
@@ -1,24 +1,39 @@
 use gloo::console::log;
+use web_sys::{File, FileList, HtmlInputElement};
 use yew::prelude::*;
 
 #[function_component(FileUpload)]
 pub fn file_upload() -> Html {
+    let filelist: UseStateHandle<Vec<File>> = use_state(|| vec![]);
+    let clone_filelist = filelist.clone();
     let ondrop = {
-        Callback::from(move |event: DragEvent|{
+        Callback::from(move |event: DragEvent| {
             event.prevent_default();
-            let files = event.data_transfer().unwrap().files();
-            log!(files);
-
+            let file = event.data_transfer().unwrap().files().unwrap().get(0);
+            log!(file.clone().unwrap());
+            let mut filelistvalue: Vec<_> = clone_filelist.to_vec();
+            filelistvalue.push(file.unwrap());
+            clone_filelist.set(filelistvalue);
+            wasm_bindgen_futures::spawn_local(async move {
+              
+            });
         })
     };
     let ondragover = {
-        Callback::from(move |event: DragEvent|{
+        Callback::from(move |event: DragEvent| {
             event.prevent_default();
         })
     };
     let ondragenter = {
-        Callback::from(move |event: DragEvent|{
-            event.prevent_default();            
+        Callback::from(move |event: DragEvent| {
+            event.prevent_default();
+        })
+    };
+
+    let onchange = {
+        Callback::from(move |event: Event| {
+            let input: HtmlInputElement = event.target_unchecked_into();
+            log!(input.files());
         })
     };
     html! {
@@ -35,6 +50,14 @@ pub fn file_upload() -> Html {
                  <p>{"Drop your images here or click to select"}</p>
                 </div>
             </label>
+            <input
+                id="file-upload"
+                type="file"
+                accept="image/*, video/*"
+                multiple={true}
+                onchange={onchange}
+            />
+            <div>{format!("{:?}", filelist)}</div>
         </div>
     }
 }
