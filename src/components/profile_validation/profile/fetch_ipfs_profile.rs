1574bbde8b74b5e4327a73dac5fc175078a803e0
Amiya Behera <amiyatulu@gmail.com>
2023-05-28 19:35:48 +0530

    view profile

--- src/components/profile_validation/profile/fetch_ipfs_profile.rs
+++ src/components/profile_validation/profile/fetch_ipfs_profile.rs
@@ -1,6 +1,6 @@
+use crate::constants::constant::IPFSFetchProvider;
 use gloo::console::log;
 use gloo_net::http::{FormData, Headers, Request};
-use crate::constants::constant::IPFSFetchProvider;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -11,12 +11,15 @@ pub struct ProfileFetchResponse {
     pub profile_video_cid: String,
 }
 
-pub async fn ipfs_fetch(hash: &str, ipfs_fetch_provider: IPFSFetchProvider<'_>) {
-    ipfs_res(hash, &ipfs_fetch_provider.address ).await;
-}
-
-pub async fn ipfs_res(hash: &str, url: &str) {
-    let resp = Request::get(&format!("{}{}", url, hash)).send().await.unwrap();
+pub async fn ipfs_fetch(
+    hash: &str,
+    ipfs_fetch_provider: IPFSFetchProvider<'_>,
+) -> ProfileFetchResponse {
+    let resp = Request::get(&format!("{}{}", ipfs_fetch_provider.address, hash))
+        .send()
+        .await
+        .unwrap();
     let body = resp.json::<ProfileFetchResponse>().await.unwrap();
-    log!(body.name)
+    // log!(body.name);
+    body
 }
--- src/components/profile_validation/profile/view_profile.rs
+++ src/components/profile_validation/profile/view_profile.rs
@@ -2,7 +2,10 @@ use crate::components::accounts::account_store::AccountPubStore;
 use crate::components::navigation::nav::Nav;
 use crate::components::profile_validation::profile::fetch_ipfs_profile::ipfs_fetch;
 use crate::constants::constant::DEFAULT_IPFS_FETCH_PROVIDER;
+use crate::components::markdown::markdown_to_html::parse_text_to_html;
+
 use gloo::console::log;
+use stylist::{yew::styled_component, Style};
 use subxt::{OnlineClient, PolkadotConfig};
 use yew::prelude::*;
 use yewdux::prelude::*;
@@ -19,16 +22,23 @@ use wasm_bindgen_futures;
 pub mod polkadot {}
 use polkadot::runtime_types::pallet_support::Content;
 
-#[function_component(ViewProfile)]
+const STYLE_FILE: &str = include_str!("view_profile.css");
+
+#[styled_component(ViewProfile)]
 pub fn view_profile() -> Html {
+    let stylesheet = Style::new(STYLE_FILE).unwrap();
     let (store, _) = use_store::<AccountPubStore>();
     let address_option = store.account_address.clone();
     let account_id = address_option.clone().unwrap();
-    let profile_hash: UseStateHandle<Option<String>> = use_state(|| None);
-    let profile_hash_clone = profile_hash.clone();
+    let profile_video_hash: UseStateHandle<Option<String>> = use_state(|| None);
+    let profile_video_hash_clone = profile_video_hash.clone();
     let account_id32 = AccountId32::from_str(&account_id).unwrap();
     let account_id32_clone = account_id32.clone();
     let error_message: UseStateHandle<Option<String>> = use_state(|| None);
+    let name_state: UseStateHandle<Option<String>> = use_state(|| None);
+    let name_state_clone = name_state.clone();
+    let details_state: UseStateHandle<Option<String>> = use_state(|| None);
+    let details_state_clone = details_state.clone();
 
     let error_message_clone1 = error_message.clone();
 
@@ -68,9 +78,11 @@ pub fn view_profile() -> Html {
                             let ipfs_hash = String::from_utf8(ipfsdata).unwrap();
                             log!("ipfs_hash", ipfs_hash.clone());
 
-                            ipfs_fetch(&ipfs_hash,DEFAULT_IPFS_FETCH_PROVIDER).await;
+                            let resp = ipfs_fetch(&ipfs_hash, DEFAULT_IPFS_FETCH_PROVIDER).await;
 
-                            profile_hash_clone.set(Some(ipfs_hash.clone()));
+                            profile_video_hash_clone.set(Some(resp.profile_video_cid.clone()));
+                            name_state_clone.set(Some(resp.name.clone()));
+                            details_state_clone.set(Some(resp.details.clone()));
                         }
                     }
                     None => {
@@ -85,9 +97,27 @@ pub fn view_profile() -> Html {
     html! {
         <>
         <Nav />
-        <div class="container">
+        <div class={classes!("container", stylesheet)}>
         if let Some(error) = &*error_message {
-            <p>{format!("{}", error)}</p>
+            <h2 class="heading">{"Error"} </h2>
+            <p class="data">{format!("{}", error)}</p>
+        }
+        if let Some(name) = &*name_state {
+            <h2 class="heading">{"Name"} </h2>
+            <p class="data">{format!("{}", name)}</p>
+        }
+        if let Some(details) = &*details_state {
+            <h2 class="heading">{"Details"} </h2>
+            <div class="data">{Html::from_html_unchecked(AttrValue::from(parse_text_to_html(&format!("{}", details))))}</div>
+        }
+
+        if profile_video_hash.is_some() {
+            <div class="col-md-6 offset-md-3 text-center">
+                <video width="320" height="240" controls={true}>
+                <source src={format!("https://ipfs.io/ipfs/{}", profile_video_hash.as_deref().unwrap_or_default())} type="video/mp4" />
+                {"Your browser does not support the video tag."}
+                </video>
+            </div>
         }
         </div>
         </>
