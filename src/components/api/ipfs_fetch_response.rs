73d35abd2dd18f42b2546e489d97327c3c4bd96b
Amiya Behera <amiyatulu@gmail.com>
2023-05-30 17:06:02 +0530

    view profile

---
+++ src/components/api/ipfs_fetch_response.rs
@@ -0,0 +1,9 @@
+use gloo_net::http::{Request, Response};
+
+pub async fn ipfs_fetch_response(hash: &str, ipfs_address: &str) -> Response {
+    let resp = Request::get(&format!("{}{}", ipfs_address, hash))
+        .send()
+        .await
+        .unwrap();
+    resp
+}
